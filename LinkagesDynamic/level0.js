058732455552ee7127a2bf806260b8c9c5c5f119
Jamie Perconti <jamieperconti@gmail.com>
2021-11-06 00:48:16 -0400

    refactor end condition for several levels to use the nice little helper method

--- LinkagesDynamic/level0.js
+++ LinkagesDynamic/level0.js
@@ -1,89 +1,82 @@
 myLevels[0] = {
-	cartesian: true,
-	instructions: "Place an adder on the board",
-	explanation: "This is a 'cartesian coordinates' adder, with one input stuck on the horizontal axis, and the other stuck on the veritcal axis...",
-	testComplete: function(){
-		return (myOperators.length==1
-			&&myOperators[0].type===ADDER);
-	}
+    cartesian: true,
+    instructions: "Place an adder on the board",
+    explanation: "This is a 'cartesian coordinates' adder, with one input stuck on the horizontal axis, and the other stuck on the veritcal axis...",
+    testComplete: function(){
+	return (myOperators.length==1
+		&&myOperators[0].type===ADDER);
+    }
 };
 
 myLevels[1] = {
-	cartesian: true,
-	instructions: "Slide the adder open to form a rectangle.",
-	testComplete: function(){
-		return (myOperators.length==1
-			&&myOperators[0].type===ADDER
-			&&myOperators[0].myInput1.real!=0
-			&&myOperators[0].myInput2.imaginary!=0);
-	}
+    cartesian: true,
+    instructions: "Slide the adder open to form a rectangle.",
+    testComplete: function(){
+	return (myOperators.length==1
+		&&myOperators[0].type===ADDER
+		&&myOperators[0].myInput1.real!=0
+		&&myOperators[0].myInput2.imaginary!=0);
+    }
 };
 
 myLevels[2] = {
-	cartesian: true,
-	DRO: true,
-	instructions: "Position the blue dot on the green dot.",
-
-	targetDot: true,
-
-	outputTargetX: -4,
-	outputTargetY: 6,
-
-	testComplete: function(){
-		if(myOperators.length==1){
-
-			let outputComplete = (dist(myOperators[0].myOutput.getRealPx(),myOperators[0].myOutput.getImaginaryPx(),axisToPixelX(this.outputTargetX),axisToPixelY(this.outputTargetY))<5);
-
-			return outputComplete;
-		}else{
-			return false;
-		}
-	}
+    cartesian: true,
+    DRO: true,
+    instructions: "Position the blue dot on the green dot.",
+    
+    targetDot: true,
+    
+    outputTargetX: -4,
+    outputTargetY: 6,
+    
+    testComplete: function() {
+	return (myOperators.length==1 &&
+		myOperators[0].myOutput.isWithinPx(this.outputTargetX,
+						   this.outputTargetY,
+						   5));
+    }
 };
 
 myLevels[3] = {
-	cartesian: true,
-	DRO: true,
-	instructions: "Positiong the blue dot on the green dot.",
-
-	targetDot: true,
-
-	outputTargetX: 8,
-	outputTargetY: -2,
-
-	testComplete: function(){
-		if(myOperators.length==1){
-
-			let outputComplete = (dist(myOperators[0].myOutput.getRealPx(),myOperators[0].myOutput.getImaginaryPx(),axisToPixelX(this.outputTargetX),axisToPixelY(this.outputTargetY))<5);
-
-			return outputComplete;
-		}else{
-			return false;
-		}
-	}
+    cartesian: true,
+    DRO: true,
+    instructions: "Positiong the blue dot on the green dot.",
+    
+    targetDot: true,
+    
+    outputTargetX: 8,
+    outputTargetY: -2,
+    
+    testComplete: function() {
+	return (myOperators.length==1 &&
+		myOperators[0].myOutput.isWithinPx(this.outputTargetX,
+						   this.outputTargetY,
+						   5));
+    }
 };
 
 myLevels[4] = {
-	DRO: true,
-	instructions: "If we turn off the cartesian coordinates constraint, we get a normal adder.\nFind\n (4,-3i) + (-12,-4i)",
-	inputTarget1X: 4,
-	inputTarget1Y: -3,
-	inputTarget2X: -12,
-	inputTarget2Y: -4,
-	outputTargetX: -8,
-	outputTargetY: -7,
-	testComplete: function(){
-		if(myOperators.length==1){
-
-			let inputComplete = (dist(myOperators[0].myInput1.getRealPx(),myOperators[0].myInput1.getImaginaryPx(),axisToPixelX(this.inputTarget1X),axisToPixelY(this.inputTarget1Y))<15)||
-								(dist(myOperators[0].myInput1.getRealPx(),myOperators[0].myInput1.getImaginaryPx(),axisToPixelX(this.inputTarget2X),axisToPixelY(this.inputTarget2Y))<15);
-			let outputComplete = (dist(myOperators[0].myOutput.getRealPx(),myOperators[0].myOutput.getImaginaryPx(),axisToPixelX(this.outputTargetX),axisToPixelY(this.outputTargetY))<15);
-
-			return inputComplete&&outputComplete;
-		}else{
-			return false;
-		}
-	}
+    DRO: true,
+    instructions: "If we turn off the cartesian coordinates constraint, we get a normal adder.\nFind\n (4,-3i) + (-12,-4i)",
+    inputTarget1X: 4,
+    inputTarget1Y: -3,
+    inputTarget2X: -12,
+    inputTarget2Y: -4,
+    outputTargetX: -8,
+    outputTargetY: -7,
+    testComplete: function(){
+	return (myOperators.length==1 &&
+		(myOperators[0].myInput1.isWithinPx(this.inputTarget1X,
+						    this.inputTarget1Y,
+						    15)
+		 ||
+		 myOperators[0].myInput1.isWithinPx(this.inputTarget2X,
+						    this.inputTarget2Y,
+						    15))
+		&& myOperators[0].myOutput.isWithinPx(this.outputTargetX,
+						      this.outputTargetY,
+						      15));
+    }
 };
 
 myLevels[5] = {
@@ -94,46 +87,42 @@ myLevels[5] = {
 };
 
 myLevels[6] = {
-	polar: true,
-	instructions: "Now place a multiplier on the board",
-	explanation: "This is a 'polar coordinates' multiplier, with one input stuck on the positive horizontal axis, and the other stuck on the unit circle...",
-	testComplete: function(){
-		return (myOperators.length==1
-			&&myOperators[0].type===MULTIPLIER);
-	}
+    polar: true,
+    instructions: "Now place a multiplier on the board",
+    explanation: "This is a 'polar coordinates' multiplier, with one input stuck on the positive horizontal axis, and the other stuck on the unit circle...",
+    testComplete: function(){
+	return (myOperators.length==1
+		&& myOperators[0].type===MULTIPLIER);
+    }
 };
 
 myLevels[7] = {
-	polar: true,
-	instructions: "Open up the multiplier...",
-	testComplete: function(){
-		return (myOperators.length==1
-			&&myOperators[0].type===MULTIPLIER
-			&&myOperators[0].myInput1.real!=1
-			&&myOperators[0].myInput2.imaginary!=0);
-	}
+    polar: true,
+    instructions: "Open up the multiplier...",
+    testComplete: function(){
+	return (myOperators.length==1
+		&&myOperators[0].type===MULTIPLIER
+		&&myOperators[0].myInput1.real!=1
+		&&myOperators[0].myInput2.imaginary!=0);
+    }
 };
 
 myLevels[8] = {
-	polar: true,
-	DRO: true,
-	instructions: "Postion the red dot on the green dot.",
-
-	targetDot: true,
-
-	outputTargetX: 3,
-	outputTargetY: 2,
-
-	testComplete: function(){
-		if(myOperators.length==1){
-
-			let outputComplete = (dist(myOperators[0].myOutput.getRealPx(),myOperators[0].myOutput.getImaginaryPx(),axisToPixelX(this.outputTargetX),axisToPixelY(this.outputTargetY))<5);
-
-			return outputComplete;
-		}else{
-			return false;
-		}
-	}
+    polar: true,
+    DRO: true,
+    instructions: "Postion the red dot on the green dot.",
+    
+    targetDot: true,
+    
+    outputTargetX: 3,
+    outputTargetY: 2,
+    
+    testComplete: function(){
+	return (myOperators.length==1 &&
+		myOperators[0].myOutput.isWithinPx(this.outputTargetX,
+						   this.outputTargetY,
+						   5));
+    }
 };
 
 myLevels[9] = {
